98a7a3b67d3eed20965407dd2be03f1febd43c68
shootneeq <40922230+shootneeq@users.noreply.github.com>
2020-07-26 01:16:42 +0200

    generalized CRUD context creation

---
+++ front/src/context/ContextGenerator.tsx
@@ -0,0 +1,75 @@
+import React, { createContext, useState, useEffect, ReactNode } from "react";
+import { generateCRUD } from "../api/apiGenerator";
+
+export interface ContextProps {
+  all: any[];
+  create: Function;
+  update: Function;
+  delete: Function;
+  getOneFromContext: Function;
+  refresh: Function;
+}
+
+export const initialContextValue = {
+  all: [],
+  update: () => {},
+  create: () => {},
+  delete: () => {},
+  getOneFromContext: () => {},
+  refresh: () => {},
+};
+
+interface Props {
+  apiBaseUrl: string;
+  children: ReactNode;
+  context: React.Context<ContextProps>;
+}
+
+function NIContext({ apiBaseUrl, children, context }: Props) {
+  const [all, setNutrition]: [any[], Function] = useState([]);
+
+  const API = generateCRUD(apiBaseUrl);
+
+  useEffect(() => {
+    refresh();
+  }, []);
+
+  async function refresh() {
+    const allNutr: any[] = await API.READ_ALL();
+    setNutrition(allNutr);
+    return all;
+  }
+
+  function getOneFromContext(id: any["_id"]) {
+    const item = all.find((item) => item._id === id);
+    return item;
+  }
+
+  async function createOne(item: any) {
+    await API.CREATE(item).then(() => refresh());
+  }
+
+  async function updateOne(item: any) {
+    await API.UPDATE_BY_ID(item).then(() => refresh());
+  }
+
+  async function deleteOne(id: any["_id"]) {
+    await API.DELETE_BY_ID(id).then(() => refresh());
+  }
+
+  const providerValues = {
+    all: all,
+    update: updateOne,
+    create: createOne,
+    delete: deleteOne,
+    getOneFromContext: getOneFromContext,
+    refresh: refresh,
+  };
+
+  return (
+    //TODO: figure out why context updates are inconsistent, especially when it comes to a refresh, following a change
+    <context.Provider value={providerValues}>{children}</context.Provider>
+  );
+}
+
+export default NIContext;
--- front/src/context/NIContext.tsx
+++ front/src/context/NIContext.tsx
@@ -1,77 +1,26 @@
-import React, { createContext, useState, useEffect, ReactNode } from "react";
-import { NutritionItem, NILogic } from "../logic/nutrition/NutritionLogic";
-import { generateCRUD } from "./../api/apiGenerator";
+import React, { createContext, ReactNode } from "react";
+import ContextGenerator, {
+  ContextProps,
+  initialContextValue,
+} from "./ContextGenerator";
 
-const API = generateCRUD("http://localhost:4000/nutritionItems");
-
-export const TestContext = createContext<any>("test context value");
-
-interface NutritionContextProps {
-  all: any[];
-  create: Function;
-  update: Function;
-  delete: Function;
-  getOneFromContext: Function;
-  refresh: Function;
-}
-
-export const NutritionContext = createContext<NutritionContextProps>({
-  all: [],
-  update: () => {},
-  create: () => {},
-  delete: () => {},
-  getOneFromContext: () => {},
-  refresh: () => {},
-});
+export const NutritionContext = createContext<ContextProps>(
+  initialContextValue
+);
 
 interface Props {
   children: ReactNode;
 }
 
 function NIContext({ children }: Props) {
-  const [all, setNutrition]: [NutritionItem[], Function] = useState([]);
-
-  useEffect(() => {
-    refresh();
-  }, []);
-
-  async function refresh() {
-    const allNutr: NutritionItem[] = await API.READ_ALL();
-    setNutrition(allNutr);
-    return all;
-  }
-
-  function getOneFromContext(id: NutritionItem["_id"]) {
-    const item = all.find((item) => item._id === id);
-    return item;
-  }
-
-  async function createOne(item: NutritionItem) {
-    await API.CREATE(item).then(() => refresh());
-  }
-
-  async function updateOne(item: NutritionItem) {
-    await API.UPDATE_BY_ID(item).then(() => refresh());
-  }
-
-  async function deleteOne(id: NutritionItem["_id"]) {
-    await API.DELETE_BY_ID(id).then(() => refresh());
-  }
-
-  const providerValues = {
-    all: all,
-    update: updateOne,
-    create: createOne,
-    delete: deleteOne,
-    getOneFromContext: getOneFromContext,
-    refresh: refresh,
-  };
-
   return (
     //TODO: figure out why context updates are inconsistent, especially when it comes to a refresh, following a change
-    <NutritionContext.Provider value={providerValues}>
+    <ContextGenerator
+      apiBaseUrl={"http://localhost:4000/nutritionItems"}
+      context={NutritionContext}
+    >
       {children}
-    </NutritionContext.Provider>
+    </ContextGenerator>
   );
 }
 
