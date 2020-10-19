16b4e767cc973fd5373db4be0fdc92b7a60c9f8d
shootneeq <40922230+shootneeq@users.noreply.github.com>
2020-10-20 00:29:08 +0200

    added BASIC_DEFAULTS

--- front/src/logic/sharedLogic.ts
+++ front/src/logic/sharedLogic.ts
@@ -9,3 +9,10 @@ export interface Basic {
   lastModifiedOn: string ;
   _v?: number ;
 }
+
+export const BASIC_DEFAULTS = {
+  _id: "not yet saved",
+  createdOn: new Date().toISOString(),
+  lastModifiedOn: new Date().toISOString(),
+  _v: -1,
+}
