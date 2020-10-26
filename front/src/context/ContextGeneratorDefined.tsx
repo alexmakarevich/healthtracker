51e50c224a526002e2d24084774f1a7c1f28ee5a
shootneeq <40922230+shootneeq@users.noreply.github.com>
2020-10-27 00:42:14 +0100

    new context generator with defined context

---
+++ front/src/context/ContextGeneratorDefined.tsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect, ReactNode } from "react";
+import { generateCRUD, WithId } from "../api/apiGenerator";
+import { createContextDefined } from "./ContextWrapper";
+
+const timestamp = () => {
+  const timestamp = new Date().toISOString();
+  return timestamp;
+};
+
+export interface ContextProps<Something extends WithId> {
+  all: Something[];
+  // likely return type: Promise<itemType>
+  create: (objectToCreate: Something) => any;
+  update: (objectToUpdate: Something) => any;
+  delete: (objectToDelete: Something) => any;
+  getOneFromContext: (idOfObjectToGet: string) => any;
+  refresh: () => void;
+}
+
+/**
+ * creates a context and its provider with standard CRUD for a collection of items
+ * make sure to provide item types
+ * make sure to give the context & provider a unique name
+ */
+export function generateDefinedContext<Item extends WithId>(
+  apiBaseUrl: string
+) {
+  const [useContextDefined, Provider] = createContextDefined<
+    ContextProps<Item>
+  >();
+
+  interface Props {
+    children: ReactNode;
+  }
+
+  function ContextProvider({ children }: Props) {
+    const [all, setAll]: [Item[], Function] = useState([]);
+
+    const API = generateCRUD(apiBaseUrl);
+
+    useEffect(() => {
+      refresh();
+    }, []);
+
+    async function refresh() {
+      const all: Item[] = await API.READ_ALL();
+      setAll(all);
+    }
+
+    function getOneFromContext(id: string) {
+      const item = all.find((item) => item._id === id);
+      return item;
+    }
+
+    async function createOne(item: Item) {
+      let result;
+      await API.CREATE(item).then((res) => {
+        refresh();
+        // console.log("res");
+        // console.log(res);
+        result = res;
+      });
+      // console.log("result");
+      // console.log(result);
+      return result;
+    }
+
+    async function updateOne(item: Item) {
+      const itemWithModifiedTimestamp = {
+        ...item,
+        lastModifiedOn: timestamp(),
+      };
+      await API.UPDATE_BY_ID(itemWithModifiedTimestamp).then(() => refresh());
+    }
+
+    async function deleteOne(item: Item) {
+      await API.DELETE_BY_ID(item._id).then(() => refresh());
+    }
+
+    const providerValues: ContextProps<Item> = {
+      all: all,
+      update: updateOne,
+      create: createOne,
+      delete: deleteOne,
+      getOneFromContext: getOneFromContext,
+      refresh: refresh,
+    };
+
+    return (
+      //TODO: figure out why context updates are inconsistent, especially when it comes to a refresh, following a change
+      <Provider value={providerValues}>{children}</Provider>
+    );
+  }
+
+  // unsure if to return declaration or result
+  return {
+    ContextProvider,
+    useContextDefined,
+  };
+}
---
+++ front/src/context/ContextWrapper.tsx
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+export function createContextDefined<ContextType>() {
+  const firstContext = createContext<ContextType | undefined>(undefined);
+
+  function useContextDefined() {
+    const c = useContext(firstContext);
+    if (!c) {
+      throw new Error(
+        "useContextDefined must be inside a Provider with a value"
+      );
+    }
+    return c;
+  }
+  return [useContextDefined, firstContext.Provider] as const;
+}
