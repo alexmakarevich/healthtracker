3f91629a5cdc086d37ff76833436f960fbd77699
shootneeq <40922230+shootneeq@users.noreply.github.com>
2021-06-23 21:13:21 +0200

    refresh back too

--- refreshSharedUnix.sh
+++ refreshSharedUnix.sh
@@ -1,3 +1,3 @@
 # this script refreshes the "shared" package
 
-yarn --cwd ./shared webpack & yarn --cwd ./front upgrade shared
+yarn --cwd ./shared webpack & yarn --cwd ./front upgrade shared & yarn --cwd ./back upgrade shared
